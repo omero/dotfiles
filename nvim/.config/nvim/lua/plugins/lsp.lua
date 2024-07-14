c8aab4a365736303cb365431262663fbcad436e4
Omar Aguirre <omersguchigu@gmail.com>
2024-07-13 22:44:34 -0600

    ✨ (nvim): Adding neo-tree and none-ls

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
 
     require('mason').setup({})
     require('mason-lspconfig').setup({
-      ensure_installed = {'tsserver', 'rust_analyzer'},
+      ensure_installed = {'tsserver', 'rust_analyzer', 'intelephense','tflint','gopls'},
       handlers = {
         function(server_name)
           require('lspconfig')[server_name].setup({
