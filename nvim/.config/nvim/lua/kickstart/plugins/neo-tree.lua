e0e433f813e8c8d7b61a9539f3c8238305bf1b95
Omar Aguirre <omersguchigu@gmail.com>
2024-08-23 23:14:48 -0600

    🚧 feat: ricing starts

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,9 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
